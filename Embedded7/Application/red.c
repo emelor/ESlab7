b3e65f67150ca1d1b9140438ff15d5e5a0ec67a1
unknown <emeliehe@mip12.MD.KTH.SE>
2012-08-24 15:16:39 +0200

    Knight Rider-effect, manual schedule switching

--- Embedded7/Application/red.c
+++ Embedded7/Application/red.c
@@ -8,8 +8,13 @@
 void redStartMain(void) {
 leds_initialise();
 blinky0Args = 0;
-redSetSchedule(&redScheduleBlinky0);
+blinky1Args = 0;
+blinky2Args = 0;
+blinky3Args = 0;
+//redSetSchedule(&redScheduleBlinky0);
+redSetSchedule(&redSchedule2);
 }
+
 void blinky0Main (int *state) {
 if (*state == 0) {
 *state = 1;
@@ -18,5 +23,41 @@ leds_turnON(0);
 else {
 *state = 0;
 leds_turnOFF(0);
+}
+}
+
+
+void blinky1Main (int *state) {
+if (*state == 0) {
+*state = 1;
+leds_turnON(1);
+}
+else {
+*state = 0;
+leds_turnOFF(1);
+}
+}
+
+
+void blinky2Main (int *state) {
+if (*state == 0) {
+*state = 1;
+leds_turnON(2);
+}
+else {
+*state = 0;
+leds_turnOFF(2);
+}
+}
+
+
+void blinky3Main (int *state) {
+if (*state == 0) {
+*state = 1;
+leds_turnON(3);
+}
+else {
+*state = 0;
+leds_turnOFF(3);
 }
 }
--- Embedded7/Application/red.h
+++ Embedded7/Application/red.h
@@ -2,4 +2,7 @@
 #define _RED_H_
 void redStartMain(void);
 void blinky0Main (int *state);
+void blinky1Main (int *state);
+void blinky2Main (int *state);
+void blinky3Main (int *state);
 #endif
